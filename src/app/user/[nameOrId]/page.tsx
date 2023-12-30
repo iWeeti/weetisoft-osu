28cb7a8bdcd2aed093cf09369371987fce345c9c
iWeeti <maaninkaveeti@gmail.com>
2023-12-30 19:42:06 +0100

    feat: top 5 scores for user

--- src/app/user/[nameOrId]/page.tsx
+++ src/app/user/[nameOrId]/page.tsx
@@ -1,9 +1,13 @@
+import { desc } from "drizzle-orm";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { BaseLayout } from "~/components/layouts/base";
 import { Separator } from "~/components/ui/separator";
 import { osuLegacy } from "~/lib/osu";
 import { db } from "~/server/db";
+import { scores } from "~/server/db/schema";
 
 export const revalidate = 60;
 
@@ -21,6 +25,15 @@ export default async function ProfilePage({
 
   const user = await db.query.users.findFirst({
     where: (u, { eq }) => eq(u.name, osuUser.username),
+    with: {
+      scores: {
+        orderBy: desc(scores.totalScore),
+        limit: 5,
+        with: {
+          beatmap: true
+        }
+      },
+    }
   });
 
   if (!user) {
@@ -89,6 +102,92 @@ export default async function ProfilePage({
           </p>
         </div>
       </div>
+      <hr className="my-5" />
+      <h3>Top 5 Scores</h3>
+      <ul className="space-y-5 my-5">
+
+        {user.scores.map((score) => {
+          const { beatmap } = score;
+
+          return (
+            <li
+              className="flex h-64 rounded-sm border p-3 shadow"
+              style={{
+                backgroundImage: `url(https://assets.ppy.sh/beatmaps/${beatmap?.beatmapSetId}/covers/cover@2x.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundColor: "rgba(0, 0, 0, 0.75)",
+                backgroundBlendMode: "darken",
+              }}
+              key={score.id}
+            >
+              <div className="flex flex-col">
+                <Link
+                  href={`https://osu.ppy.sh/b/${beatmap?.beatmapId}#osu/${beatmap?.beatmapSetId}`}
+                  target={"_blank"}
+                  className="flex-none hover:underline"
+                >
+                  {beatmap?.beatmapName ?? "Unable to load"}
+                </Link>
+                <time className="flex-none text-sm text-muted-foreground">
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "medium",
+                    timeStyle: "medium",
+                  }).format(new Date(score.time))}
+                  {" • "}
+                  {new Intl.RelativeTimeFormat("en-US", {
+                    numeric: "auto",
+                  }).format(
+                    Math.floor(
+                      (new Date(score.time).getTime() - Date.now()) / 1000 / 60 / 60,
+                    ),
+                    "hour",
+                  )}
+                </time>
+                <div className="flex-1"></div>
+                <div className="flex flex-none items-center gap-2">
+                  <p className="text-lg">
+                    {unitFormat.format(score.totalScore)}
+                  </p>
+                  <div className="h-4 w-px border border-muted-foreground"></div>
+                  <p className="text-lg text-muted-foreground">{unitFormat.format(score.maxCombo)}x</p>
+
+                  <div className="h-4 w-px border border-muted-foreground"></div>
+                  <p className="text-lg text-muted-foreground">Placed #{unitFormat.format(score.rank)} in the lobby</p>
+                </div>
+              </div>
+              <div className="ml-auto flex flex-col">
+                <div className="grid grid-cols-2 gap-2 text-sm text-muted-foreground">
+                  <p>
+                    {unitFormat.format(score.count300)}
+                  </p>
+                  <p>
+                    x <span className="text-blue-500">300</span>
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {unitFormat.format(score.count100)}
+                  </p>
+                  <p>
+                    x <span className="text-green-500">100</span>
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {unitFormat.format(score.count50)}
+                  </p>
+                  <p>
+                    x <span className="text-orange-500">50</span>
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {unitFormat.format(score.countMiss)}
+                  </p>
+                  <p>
+                    x <span className="text-gray-500">miss</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,6 +4,7 @@ import {
   sqliteTable,
   text,
   primaryKey,
+  real,
 } from "drizzle-orm/sqlite-core";
 import type { AdapterAccount } from "@auth/core/adapters";
 
@@ -45,7 +46,7 @@ export const playerBansRelations = relations(playerBans, ({ one }) => ({
 }));
 
 export const users = sqliteTable("Users", {
-  id: text("Id").notNull().primaryKey(),
+  id: integer("Id").notNull().primaryKey(),
   userId: integer("UserId"),
   name: text("Name").notNull(),
   playtime: integer("Playtime").notNull(),
@@ -61,7 +62,62 @@ export const users = sqliteTable("Users", {
 
 export const usersRelations = relations(users, ({ many }) => ({
   playerBans: many(playerBans),
+  scores: many(scores),
 }));
+
+export const maps = sqliteTable("Maps", {
+  id: integer("Id", {
+    mode: "number"
+  }).notNull().primaryKey({
+    autoIncrement: true,
+  }),
+  beatmapId: integer("BeatmapId").notNull(),
+  beatmapSetId: integer("BeatmapSetId").notNull(),
+  beatmapName: text("BeatmapName").notNull(),
+  beatmapArtist: text("BeatmapArtist").notNull(),
+  difficultyName: text("DifficultyName").notNull(),
+  starRating: real("StarRating"),
+  timesPlayed: integer("TimesPlayed").notNull(),
+  averagePassPercentage: real("AveragePassPercentage"),
+  averageLeavePercentage: real("AverageLeavePercentage"),
+  lastPlayed: text("LastPlayed").notNull(),
+});
+
+export const mapsRelations = relations(maps, ({one, many}) => ({
+  scores: many(scores)
+}))
+
+export const scores = sqliteTable("Scores", {
+  id: integer("Id").notNull().primaryKey({
+    autoIncrement: true,
+  }),
+  userId: integer("UserId").notNull().references(() => users.id),
+  gameId: integer("gameId").notNull(),
+  playerId: integer("PlayerId"),
+  lobbyId: integer("LobbyId").notNull(),
+  osuScoreId: integer("OsuScoreId"),
+  beatmapId: integer("BeatmapId").notNull().references(() => maps.beatmapId),
+  totalScore: integer("TotalScore").notNull(),
+  rank: integer("Rank").notNull(),
+  maxCombo: integer("MaxCombo").notNull(),
+  count300: integer("Count300").notNull(),
+  count100: integer("Count100").notNull(),
+  count50: integer("Count50").notNull(),
+  countMiss: integer("CountMiss").notNull(),
+  mods: integer("Mods").notNull(),
+  time: text("Time").notNull(),
+});
+
+export const scoresRelations = relations(scores, ({one}) => ({
+  user: one(users, {
+    fields: [scores.userId],
+    references: [users.id]
+  }),
+  beatmap: one(maps, {
+    fields: [scores.beatmapId],
+    references: [maps.beatmapId]
+  })
+}))
 
 export const websiteUsers = sqliteTable("user", {
   id: text("id").notNull().primaryKey(),
